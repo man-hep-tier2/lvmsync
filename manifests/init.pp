f0d5843ee25801c77ef0140010e1097d4b17f17f
Robert Frank <robert.frank@manchester.ac.uk>
2019-11-05 14:03:57 +0000

    Add link for lvmsync.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,4 +3,8 @@ class lvmsync {
     provider => 'puppet_gem',
     ensure   => 'present',
   }
+  file { '/opt/puppetlabs/bin/lvmsync':
+    ensure => 'link',
+    target => '/opt/puppetlabs/puppet/bin/lvmsync',
+  }
 }
